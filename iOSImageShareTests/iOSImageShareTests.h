28fb84c68dd57f4ba53411c68720bc567e3fe44b
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2011-05-08 12:16:32 -0300

    first commit

---
+++ iOSImageShareTests/iOSImageShareTests.h
@@ -0,0 +1,17 @@
+//
+//  iOSImageShareTests.h
+//  iOSImageShareTests
+//
+//  Created by Gustavo Ambrozio on 8/5/11.
+//  Copyright 2011 CodeCrop Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface iOSImageShareTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ iOSImageShareTests/iOSImageShareTests.m
@@ -0,0 +1,33 @@
+//
+//  iOSImageShareTests.m
+//  iOSImageShareTests
+//
+//  Created by Gustavo Ambrozio on 8/5/11.
+//  Copyright 2011 CodeCrop Software. All rights reserved.
+//
+
+#import "iOSImageShareTests.h"
+
+
+@implementation iOSImageShareTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iOSImageShareTests");
+}
+
+@end
