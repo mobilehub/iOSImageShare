bfc0165e45aadcc090f134b63fa8b49a7c9b2748
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2011-05-11 13:13:59 -0300

    File to hold some constants that can be changed by any given app.

---
+++ iOSImageShare/iOSImageShareConstants.h
@@ -0,0 +1,9 @@
+//
+//  iOSImageShareConstants.h
+//  iOSImageShare
+//
+//  Created by Gustavo Ambrozio on 11/5/11.
+//
+
+#define SCHEMAS_URL  @"https://github.com/gpambrozio/iOSImageShare/raw/master/iOSImageShare/CompatibleApps.txt"
+#define GENERIC_APP_ICON   @"sharer-generic.png"
